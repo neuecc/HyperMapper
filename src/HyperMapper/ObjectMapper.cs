941d77ff61296de74fffb6b7588cb169b981cd8e
Yoshifumi Kawai <ils@neue.cc>
2018-02-16 23:50:53 +0900

    chain struct

--- src/HyperMapper/ObjectMapper.cs
+++ src/HyperMapper/ObjectMapper.cs
@@ -51,10 +51,50 @@ namespace HyperMapper
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T DeepCopy<T>(T cloneFrom, IObjectMapperResolver resolver)
+        {
+            return resolver.GetMapperWithVerify<T, T>().Map(cloneFrom, resolver);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TTo Map<TFrom, TTo>(TFrom from)
         {
             var resolver = DefaultResolver;
             return resolver.GetMapperWithVerify<TFrom, TTo>().Map(from, resolver);
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static TTo Map<TFrom, TTo>(TFrom from, IObjectMapperResolver resolver)
+        {
+            return resolver.GetMapperWithVerify<TFrom, TTo>().Map(from, resolver);
+        }
+
+        public static Chain<TFrom> From<TFrom>(TFrom from)
+        {
+            return new Chain<TFrom>(from, DefaultResolver);
+        }
+
+        public static Chain<TFrom> From<TFrom>(TFrom from, IObjectMapperResolver resolver)
+        {
+            return new Chain<TFrom>(from, resolver);
+        }
+
+        public struct Chain<T>
+        {
+            readonly T from;
+            readonly IObjectMapperResolver resolver;
+
+            public Chain(T t, IObjectMapperResolver resolver)
+            {
+                this.from = t;
+                this.resolver = resolver;
+            }
+
+            public TTo To<TTo>()
+            {
+                return ObjectMapper.Map<T, TTo>(from, resolver);
+            }
+        }
     }
+
 }
